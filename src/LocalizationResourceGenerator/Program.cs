fe46400ee4925e2c62eb8027e6fc779213a03507
hishamco <hishamco_2007@yahoo.com>
2016-10-17 16:11:15 +0300

    Add 'type' command option to support varity of sources

--- src/LocalizationResourceGenerator/Program.cs
+++ src/LocalizationResourceGenerator/Program.cs
@@ -13,6 +13,7 @@ namespace LocalizationResourceGenerator
         private static ILogger _logger;
         private static readonly string _appKey = "6CE9C85A41571C050C379F60DA173D286384E0F2";
         private static readonly string _defaultCulture = "en";
+        private static readonly string _defaultType = "resx";
         private static readonly LanguageServiceClient client = new LanguageServiceClient(EndpointConfiguration.BasicHttpBinding_LanguageService);
 
         public static void Main(string[] args)
@@ -25,18 +26,18 @@ namespace LocalizationResourceGenerator
             app.HandleResponseFiles = true;
             app.HelpOption("-h|--help");
 
-            var culturesCommand = app.Argument("cultures", "List of cultures, that the 'dotnet-resgen' command will generate a resource file(s) for each one for them.", true);
+            var culturesCommand = app.Argument("cultures", "List of cultures, that the 'dotnet-resgen' command will generate a resource file(s) for each one for them", true);
             var defaultCultureOption = app.Option("--default <CULTURE>", "The default culture that the 'dotnet-resgen' command will use it to translate from", CommandOptionType.SingleValue);
+            var resourceTypeOption = app.Option("-t|--type <TYPE>", "The type of the resource file [resx|restext]", CommandOptionType.SingleValue);
 
             _loggerFactory.AddConsole();
             _logger = _loggerFactory.CreateLogger(nameof(Program));
 
             app.OnExecute(async () =>
             {
-                const string resourceExtension = ".resx";
                 var defaultCulture = defaultCultureOption.Value() ?? _defaultCulture;
                 var cultures = culturesCommand.Values;
-                var currentDirectory = Directory.GetCurrentDirectory();
+                var resourceType = resourceTypeOption.Value() ?? _defaultType;
 
                 if (culturesCommand.Values.Count == 0)
                 {
@@ -44,37 +45,55 @@ namespace LocalizationResourceGenerator
                     return 1;
                 }
 
-                foreach (var filePath in Directory.GetFiles(currentDirectory, "*" + resourceExtension))
+                switch (resourceType)
                 {
-                    var file = new FileInfo(filePath);
+                    case "resx":
+                    case "restext":
+                        var resourceExtension = resourceType;
+                        var currentDirectory = Directory.GetCurrentDirectory();
+                        XDocument doc = null;
 
-                    if (!Path.GetFileNameWithoutExtension(file.Name).Contains("."))
-                    {
-                        foreach (var culture in cultures)
+                        foreach (var filePath in Directory.GetFiles(currentDirectory, "*." + resourceExtension))
                         {
-                            var resourceFileName = string.Join(".", Path.GetFileNameWithoutExtension(file.Name), culture, resourceExtension);
-                            var resourcePath = Path.Combine(currentDirectory, resourceFileName);
+                            var file = new FileInfo(filePath);
 
-                            File.Copy(file.FullName, resourcePath, true);
-                            var doc = XDocument.Load(resourcePath);
-
-                            foreach (var element in doc.Root.Elements("data"))
+                            if (!Path.GetFileNameWithoutExtension(file.Name).Contains("."))
                             {
-                                var key = element.Element("value").Value;
-                                var result = await client.TranslateAsync(_appKey, key.ToString(), defaultCulture, culture);
+                                foreach (var culture in cultures)
+                                {
+                                    var resourceFileName = string.Join(".", Path.GetFileNameWithoutExtension(file.Name), culture, resourceExtension);
+                                    var resourcePath = Path.Combine(currentDirectory, resourceFileName);
 
-                                element.SetElementValue("value", result);
-                            }
+                                    if (resourceExtension == "resx")
+                                    {
+                                        doc = XDocument.Load(resourcePath);
+                                        File.Copy(file.FullName, resourcePath, true);
+                                    }
+                                    else
+                                    {
+                                        doc = ResourceTextFile.Load(file.OpenRead());
+                                    }
 
-                            using (var stream = new FileStream(resourcePath, FileMode.Open, FileAccess.Write))
-                            {
-                                doc.Save(stream);
+                                    foreach (var element in doc.Root.Elements("data"))
+                                    {
+                                        var key = element.Element("value").Value;
+                                        var result = await client.TranslateAsync(_appKey, key.ToString(), defaultCulture, culture);
+
+                                        element.SetElementValue("value", result);
+                                    }
+
+                                    using (var stream = new FileStream(resourcePath, FileMode.Open, FileAccess.Write))
+                                    {
+                                        doc.Save(stream);
+                                    }
+                                }
                             }
                         }
-                    }
-                }
 
-                return 0;
+                        return 0;
+                    default:
+                        return 1;
+                }
             });
 
             app.Execute(args);
