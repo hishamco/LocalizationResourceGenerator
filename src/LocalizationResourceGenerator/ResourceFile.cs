84beab5eaee4d1f8bb0bd13a92ded850c1d69e91
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2018-11-28 18:37:56 +0300

    xproj -> cproj

---
+++ src/LocalizationResourceGenerator/ResourceFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace LocalizationResourceGenerator
+{
+    internal class ResourceFile
+    {
+        public ResourceFile(FileInfo file, ResourceFileType type)
+        {
+            File = file;
+            Type = type;
+        }
+
+        public FileInfo File { get; }
+        public ResourceFileType Type { get; }
+
+        public static ResourceFile Create(string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+            ResourceFileType type;
+            var extension = fileInfo.Extension.ToLowerInvariant();
+
+            switch (extension)
+            {
+                case ".resx":
+                    type = ResourceFileType.Resx;
+                    break;
+                case ".restext":
+                    type = ResourceFileType.Restext;
+                    break;
+                default:
+                    throw new NotSupportedException($"Unsupported resource file with extension '{extension}'.");
+            }
+
+            return new ResourceFile(fileInfo, type);
+        }
+    }
+}
